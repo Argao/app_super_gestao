bc8964f92dca611c9ca75e7468278df3cc67bf43
Aragao <jgabrielaragao@gmail.com>
2024-06-20 19:25:06 -0300

    personalizando erros

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -5,24 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\SiteContato;
 use App\MotivoContato;
+
 class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
-        return view('site.contato',['titulo' => 'Contato', 'motivo_contatos' => MotivoContato::all()]);
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => MotivoContato::all()]);
     }
 
     public function salvar(Request $request)
     {
         //realizar a validação dos dados
-        $request->validate([
-            'nome'=> 'required|min:3|max:40',
-            'telefone'=> 'required',
-            'email'=> 'email',
-            'motivo_contatos_id'=> 'required',
-            'mensagem'=> 'required|max:2000',
-
-        ]);
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000',
+        ];
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres',
+            'email.email' => 'O campo email precisa ser um email válido',
+            'mensagem.max' => 'O campo mensagem precisa ter no máximo 2000 caracteres',
+            'required' => 'O campo :attribute precisa ser preenchido',
+        ];
+        $request->validate($regras, $feedback);
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,11 +2,13 @@
 <form action="{{route('site.contato')}}" method="post">
     @csrf
     <input type="text" placeholder="Nome" value="{{ old('nome') }}" class={{$classe}} name="nome" id="nome">
-    <br>
+    @if($errors->has('nome')) <p class="erro">{{$errors->first('nome')}}</p> @else <br> @endif
+
     <input type="text" placeholder="Telefone" value="{{ old('telefone') }}" class={{$classe}} name="telefone" id="telefone">
-    <br>
+    @if($errors->has('telefone')) <p class="erro">{{$errors->first('telefone')}}</p> @else <br> @endif
+
     <input type="text" placeholder="E-mail" value="{{ old('email') }}" class={{$classe}} name="email" id="email">
-    <br>
+    @if($errors->has('email')) <p class="erro">{{$errors->first('email')}}</p> @else <br> @endif
 
     <select class={{$classe}} name="motivo_contatos_id" id="motivo_contato">
         <option value="">Qual o motivo do contato?</option>
@@ -14,15 +16,20 @@
         @foreach($motivo_contatos as $key => $motivo_contato)
             <option value="{{$motivo_contato->id}}" {{ old('motivo_contatos_id') == $motivo_contato->id ? 'selected' : '' }}> {{$motivo_contato->motivo_contato}}</option>
         @endforeach
-
     </select>
-    <br>
-    <textarea class={{$classe}} name="mensagem" id="mensagem">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem'}}
-    </textarea>
-    <br>
+    @if($errors->has('motivo_contatos_id')) <p class="erro">{{$errors->first('motivo_contatos_id')}}</p> @else <br> @endif
+
+    <textarea class={{$classe}} name="mensagem" id="mensagem">{{ (old('mensagem') != '') ? old('mensagem') : 'Preencha aqui a sua mensagem'}}</textarea>
+    @if($errors->has('mensagem')) <p class="erro">{{$errors->first('mensagem')}}</p> @else <br> @endif
+
     <button type="submit" class={{$classe}}>ENVIAR</button>
 </form>
-<div style="position: absolute; top: 0; left: 0; width: 100%; background: red">
-    <pre>{{ print_r($errors) }}</pre>
-</div>
+
+{{--@if($errors->any())--}}
+{{--    <div style="position: absolute; top: 0; left: 0; width: 100%; background: red">--}}
+{{--        @foreach($errors->all() as  $erro)--}}
+{{--            {{ $erro }}<br>--}}
+{{--        @endforeach--}}
+{{--    </div>--}}
+{{--@endif--}}
 
