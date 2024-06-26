312b50e7db9a69e3742a72435c6fa169c90f39cb
Aragao <jgabrielaragao@gmail.com>
2024-06-26 16:45:10 -0300

    validação de dados cadastro produto

--- resources/views/app/produto/create.blade.php
+++ resources/views/app/produto/create.blade.php
@@ -27,27 +27,27 @@
                     <!-- Mensagem de Sucesso ou Erro -->
 
 
-                    <input type="text" name="nome" placeholder="Nome" value="" class="borda-preta">
+                    <input type="text" name="nome" placeholder="Nome" value="{{ old('nome') }}" class="borda-preta">
                     <!-- Mensagem de Erro -->
+                    <span class="erro">{{$errors->has('nome') ? $errors->first('nome') : ''}}</span>
 
-                    <input type="text" name="descricao" placeholder="Descrição" value="" class="borda-preta">
+                    <input type="text" name="descricao" placeholder="Descrição" value="{{old('descricao')}}" class="borda-preta">
                     <!-- Mensagem de Erro -->
+                    <span class="erro">{{$errors->has('descricao') ? $errors->first('descricao') : ''}}</span>
 
-                    <input type="number" name="peso" placeholder="Peso" value="" class="borda-preta">
+                    <input type="number" name="peso" placeholder="Peso" value="{{old('peso')}}" class="borda-preta">
                     <!-- Mensagem de Erro -->
-
+                    <span class="erro">{{$errors->has('peso') ? $errors->first('peso') : ''}}</span>
 
                     <select name="unidade_id">
                         <option> -- Selecione a Unidade de Medida --</option>
 
                         @foreach($unidades as $unidade)
-                            <option value="{{$unidade->id}}">{{$unidade->descricao}}</option>
+                            <option value="{{$unidade->id}}" {{old('unidade_id') == $unidade->id ? 'selected' : ''}}>{{$unidade->descricao}}</option>
                         @endforeach
-
-
                     </select>
-                    <!-- Mensagem de Erro -->
-{{--                    <span class="erro">{{$errors->has('email') ? $errors->first('email') : ''}}</span>--}}
+                    <span class="erro">{{$errors->has('unidade_id') ? $errors->first('unidade_id') : ''}}</span>
+
 
                     <button type="submit">Cadastrar</button>
 
