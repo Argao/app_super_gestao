afbd89eeffb29698c8a6dfaaf6f7cb617adfa087
Aragao <jgabrielaragao@gmail.com>
2024-07-01 23:07:41 -0300

    hasOne e belongTo

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -19,6 +19,7 @@ class ProdutoController extends Controller
     {
         $produtos = Produto::paginate(10);
 
+/*
         foreach ($produtos as $key => $produto){
             $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
 
@@ -28,7 +29,7 @@ class ProdutoController extends Controller
                 $produtos[$key]['largura'] = $produtoDetalhe->largura;
                 $produtos[$key]['altura'] = $produtoDetalhe->altura;
             }
-        }
+        }*/
 
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all()]);
     }
--- resources/views/app/produto_detalhe/edit.blade.php
+++ resources/views/app/produto_detalhe/edit.blade.php
@@ -17,6 +17,11 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin: 0 auto 0 auto;">
+
+                <h4>Produto</h4>
+                <div>Nome: {{$produto_detalhe->produto->nome}} </div>
+                <br>
+                <div>Descrição: {{$produto_detalhe->produto->descricao}}</div>
                 @component('app.produto_detalhe._components.form_create_edit',['produto_detalhe'=>$produto_detalhe,'unidades' => $unidades])
                 @endcomponent
             </div>
